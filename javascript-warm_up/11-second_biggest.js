63f0b80aab7093b539f46b51997d2e0c2759e05a
Your Name <your-email@example.com>
2026-06-08 00:01:30 -0700

    Add JavaScript warm up tasks

---
+++ javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+if (process.argv.length <= 3) {
+  console.log(0);
+} else {
+  const args = process.argv.slice(2).map(Number);
+  args.sort(function (a, b) { return b - a; });
+  console.log(args[1]);
+}
